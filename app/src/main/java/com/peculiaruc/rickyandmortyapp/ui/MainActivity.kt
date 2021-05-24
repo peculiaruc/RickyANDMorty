521f90cff977a8e790b5eb6025972ab12691fb92
Peculiar Umeh <35475543+peculiaruc@users.noreply.github.com>
2021-05-24 19:21:02 +0100

    implemented viewmodel

--- app/src/main/java/com/peculiaruc/rickyandmortyapp/ui/MainActivity.kt
+++ app/src/main/java/com/peculiaruc/rickyandmortyapp/ui/MainActivity.kt
@@ -3,10 +3,29 @@ package com.peculiaruc.rickyandmortyapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.peculiaruc.rickyandmortyapp.R
+import com.peculiaruc.rickyandmortyapp.databinding.ActivityMainBinding
+import java.lang.reflect.Array.newInstance
+import java.net.URLClassLoader.newInstance
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+//        // 1
+//        if (savedInstanceState == null) {
+//            // 2
+//            supportFragmentManager
+//                // 3
+//                .beginTransaction()
+//                // 4
+//                .add(R.id.mainFragment, MainFragment.newInstance(), "charactList")
+//                // 5
+//                .commit()
+//        }
     }
 }
